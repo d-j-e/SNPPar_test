4a13bd3a5f02fa2c855584912091618de15676dc
David Edwards <David.Edwards@monash.edu>
2020-07-09 09:43:26 +1000

    Update countMEbyGene.py

--- code/countMEbyGene.py
+++ code/countMEbyGene.py
@@ -2,7 +2,7 @@
 #
 # 
 # ****Function Description***
-#
+# Counts number of mutation events per gene
 #
 # Author(s) 
 #	D. J. Edwards (David.Edwards@monash.edu) 
@@ -12,8 +12,8 @@
 python countMEbyGene.py -h
 '''
 #
-# Last modified - 12/02/2020
-#
+# Last modified - 09/07/2020
+# Change - simplified output (no reporting of codons and sites beyond count)
 
 from Bio import SeqIO
 from Bio.Seq import Seq
@@ -187,48 +187,50 @@ def countEvents(mutation_events_file,output):
 
 	for i in range(len(genes)):
 		output += genes[i] + '\t' + str(gene_counts[i]) + '\t' + str(gene_counts_Syn[i]) + '\t'
-		output += str(gene_counts_NonSyn[i]) + '\t' + str(gene_counts_Ambiguous[i]) + '\t' + str(len(gene_hits[i])) + '\t' + str(len(gene_Codons[i])) + '\t'
-		gene_hits_out = ''
-		gene_hits_counts_out = ''
-		gene_hits_NonSyn_counts_out = ''
-		gene_hits_Syn_counts_out = ''
-		gene_hits_Amb_counts_out = ''
-		for j in range(len(gene_hits[i])):
-			gene_hits_out += str(gene_hits[i][j])
-			gene_hits_counts_out += str(gene_hits_counts[i][j])
-			gene_hits_NonSyn_counts_out += str(gene_hits_NonSyn_counts[i][j])
-			gene_hits_Syn_counts_out += str(gene_hits_Syn_counts[i][j])
-			gene_hits_Amb_counts_out += str(gene_hits_Amb_counts[i][j])
-			if j != len(gene_hits[i])-1:
-				gene_hits_out += ","
-				gene_hits_counts_out += ","
-				gene_hits_NonSyn_counts_out += ","
-				gene_hits_Syn_counts_out += ","
-				gene_hits_Amb_counts_out += ","
-		output += gene_hits_out +'\t'+ gene_hits_counts_out + '\t' + gene_hits_NonSyn_counts_out + '\t' + gene_hits_Syn_counts_out + '\t' + gene_hits_Amb_counts_out + '\t'
-		gene_Codons_out = ''
-		gene_Codons_counts_out = ''
-		gene_Codons_NonSyn_counts_out = ''
-		gene_Codons_Syn_counts_out = ''
-		gene_Codons_Amb_counts_out = ''
-		for j in range(len(gene_Codons[i])):
-			gene_Codons_out += str(gene_Codons[i][j])
-			gene_Codons_counts_out += str(gene_Codons_counts[i][j])
-			gene_Codons_NonSyn_counts_out += str(gene_Codons_NonSyn_counts[i][j])
-			gene_Codons_Syn_counts_out += str(gene_Codons_Syn_counts[i][j])
-			gene_Codons_Amb_counts_out += str(gene_Codons_Amb_counts[i][j])
-			if j != len(gene_Codons[i])-1:
-				gene_Codons_out += ","
-				gene_Codons_counts_out += ","
-				gene_Codons_NonSyn_counts_out += ","
-				gene_Codons_Syn_counts_out += ","
-				gene_Codons_Amb_counts_out += ","
-		output += gene_Codons_out + '\t' + gene_Codons_counts_out + '\t' + gene_Codons_NonSyn_counts_out + '\t' + gene_Codons_Syn_counts_out + '\t' + gene_Codons_Amb_counts_out + '\n'
+#		output += str(gene_counts_NonSyn[i]) + '\t' + str(gene_counts_Ambiguous[i]) + '\t' + str(len(gene_hits[i])) + '\t' + str(len(gene_Codons[i])) + '\t'
+		output += str(gene_counts_NonSyn[i]) + '\t' + str(gene_counts_Ambiguous[i]) + '\t' + str(len(gene_hits[i])) + '\t' + str(len(gene_Codons[i])) + '\n'
+#		gene_hits_out = ''
+#		gene_hits_counts_out = ''
+#		gene_hits_NonSyn_counts_out = ''
+#		gene_hits_Syn_counts_out = ''
+#		gene_hits_Amb_counts_out = ''
+#		for j in range(len(gene_hits[i])):
+#			gene_hits_out += str(gene_hits[i][j])
+#			gene_hits_counts_out += str(gene_hits_counts[i][j])
+#			gene_hits_NonSyn_counts_out += str(gene_hits_NonSyn_counts[i][j])
+#			gene_hits_Syn_counts_out += str(gene_hits_Syn_counts[i][j])
+#			gene_hits_Amb_counts_out += str(gene_hits_Amb_counts[i][j])
+#			if j != len(gene_hits[i])-1:
+#				gene_hits_out += ","
+#				gene_hits_counts_out += ","
+#				gene_hits_NonSyn_counts_out += ","
+#				gene_hits_Syn_counts_out += ","
+#				gene_hits_Amb_counts_out += ","
+#		output += gene_hits_out +'\t'+ gene_hits_counts_out + '\t' + gene_hits_NonSyn_counts_out + '\t' + gene_hits_Syn_counts_out + '\t' + gene_hits_Amb_counts_out + '\t'
+#		gene_Codons_out = ''
+#		gene_Codons_counts_out = ''
+#		gene_Codons_NonSyn_counts_out = ''
+#		gene_Codons_Syn_counts_out = ''
+#		gene_Codons_Amb_counts_out = ''
+#		for j in range(len(gene_Codons[i])):
+#			gene_Codons_out += str(gene_Codons[i][j])
+#			gene_Codons_counts_out += str(gene_Codons_counts[i][j])
+#			gene_Codons_NonSyn_counts_out += str(gene_Codons_NonSyn_counts[i][j])
+#			gene_Codons_Syn_counts_out += str(gene_Codons_Syn_counts[i][j])
+#			gene_Codons_Amb_counts_out += str(gene_Codons_Amb_counts[i][j])
+#			if j != len(gene_Codons[i])-1:
+#				gene_Codons_out += ","
+#				gene_Codons_counts_out += ","
+#				gene_Codons_NonSyn_counts_out += ","
+#				gene_Codons_Syn_counts_out += ","
+#				gene_Codons_Amb_counts_out += ","
+#		output += gene_Codons_out + '\t' + gene_Codons_counts_out + '\t' + gene_Codons_NonSyn_counts_out + '\t' + gene_Codons_Syn_counts_out + '\t' + gene_Codons_Amb_counts_out + '\n'
 
 	return output
 
 def countMutationEvents(arguments):
-	output = "Gene_Tag\tME\tSyn\tPA\tAmb\tSites\tCodons\tSites_list\tSite_Counts\tPA_Site_Counts\tSyn_Site_Counts\tAmb_Site_Counts\tCodons_list\tCodon_Counts\tPA_Codon_Counts\tSyn_Codon_Counts\tAmb_Codon_Counts\n"
+#	output = "Gene_Tag\tME\tSyn\tPA\tAmb\tSites\tCodons\tSites_list\tSite_Counts\tPA_Site_Counts\tSyn_Site_Counts\tAmb_Site_Counts\tCodons_list\tCodon_Counts\tPA_Codon_Counts\tSyn_Codon_Counts\tAmb_Codon_Counts\n"
+	output = "Gene_Tag\tME\tSyn\tPA\tAmb\tSites\tCodons\n"
 	mutation_events_file = readInput(arguments.input_file)
 	output = countEvents(mutation_events_file,output)
 	writeOutput(arguments.output_prefix+"ME_counts_by_gene.txt",output)
